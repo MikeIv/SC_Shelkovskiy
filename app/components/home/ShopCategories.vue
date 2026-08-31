0eda2d7f93f819428b3ad3084cd71343c7d4d105
Mike Ivanov <gagarahome@yandex.ru>
2026-08-31 11:01:16 +0300

    fix: remove shop categories section heading from homepage

--- app/components/home/ShopCategories.vue
+++ app/components/home/ShopCategories.vue
@@ -7,14 +7,8 @@ const { items } = defineProps<{
 </script>
 
 <template>
-  <section :class="$style.root" aria-labelledby="home-shop-categories-title">
-    <div :class="$style.inner">
-      <h2 id="home-shop-categories-title" :class="$style.title">
-        Категории магазина
-      </h2>
-
-      <HomeShopCategory :items="items" />
-    </div>
+  <section :class="$style.root" aria-label="Категории магазина">
+    <HomeShopCategory :items="items" />
   </section>
 </template>
 
@@ -22,29 +16,13 @@ const { items } = defineProps<{
 @use 'tools' as *;
 
 .root {
+  max-width: var(--fs-grid-content-max);
+  margin-inline: auto;
   padding-block: rem(60) rem(100);
+  padding-inline: var(--fs-grid-margin);
 
   @include from-desktop {
     padding-block: rem(80) rem(140);
   }
-}
-
-.inner {
-  display: flex;
-  flex-direction: column;
-  gap: var(--fs-space-3);
-  max-width: var(--fs-grid-content-max);
-  margin-inline: auto;
-  padding-inline: var(--fs-grid-margin);
-
-  @include from-desktop {
-    gap: var(--fs-space-5);
-  }
-}
-
-.title {
-  margin: 0;
-  @include fs-h1;
-  color: var(--fs-color-black);
 }
 </style>
